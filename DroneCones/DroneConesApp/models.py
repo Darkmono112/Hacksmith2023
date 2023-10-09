e0687b97f2a41a2c1e530a9c2ac6120b97b1c409
Ben Smith <bnsmth235@gmail.com>
2023-10-09 12:12:17 -0600

    Models, Migrations, and Admin register

--- DroneCones/DroneConesApp/models.py
+++ DroneCones/DroneConesApp/models.py
@@ -1,14 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+DRONE_SIZES = [
+    ('Small', '1'),
+    ('Medium', '4'),
+    ('Large', '8')
+]
+class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    # We don't need a total_price param, we should just add it up in the view
+
+class Order_Item(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    flavor = models.CharField(max_length=50)
+    cone = models.CharField(max_length=50)
+    topping = models.CharField(max_length=50)
+
+class Ice_Cream(models.Model):
+    quantity = models.IntegerField()
+    flavor = models.CharField(max_length=50)
+    # Price in cents to prevent rounding errors. Format in template
+    price = models.IntegerField()
+
+class Cone(models.Model):
+    quantity = models.IntegerField()
+    flavor = models.CharField(max_length=50)
+    # Price in cents to prevent rounding errors. Format in template
+    price = models.IntegerField()
+
+class Topping(models.Model):
+    quantity = models.IntegerField()
+    flavor = models.CharField(max_length=50)
+    # Price in cents to prevent rounding errors. Format in template
+    price = models.IntegerField()
+
+class Drone(models.Model):
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    size = models.CharField(max_length=10, choices=DRONE_SIZES)
+    active = models.BooleanField(default=True)
+    on_order = models.BooleanField(default=False)
+
+    def get_capacity(self):
+        for choice in self._meta.get_field("size").choices:
+            if choice[0] == self.size:
+                return choice[1]
+        return ""
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
- 
-
-# Create your models here.
