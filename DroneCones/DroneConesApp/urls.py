1e7892d56ae43427f9feace863ee55df6b4ead58
Ben Smith <bnsmth235@gmail.com>
2023-10-13 11:20:55 -0600

    Added Order History pag

--- DroneCones/DroneConesApp/urls.py
+++ DroneCones/DroneConesApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import views, account
+from .views import views, account, orders
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -9,5 +9,8 @@ urlpatterns = [
     path("payment/", views.payment, name="payment"),
     path("flyerportal", views.flyerportal, name="flyerportal"),
     path("account/", account.account, name="account"),
-
+    path("order_history", orders.order_history, name="order_history"),
+    path("delete_order/<int:order_id>/", orders.delete_order, name="delete_order"),
 ]
+
+app_name = "DroneConesApp"
