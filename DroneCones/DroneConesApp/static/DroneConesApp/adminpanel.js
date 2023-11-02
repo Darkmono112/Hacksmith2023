424d0fe4df3ac4dd38507832975d9d4688c40230
hamnerben <hamnerben@gmail.com>
2023-11-01 18:51:09 -0600

    selected button style

--- DroneCones/DroneConesApp/static/DroneConesApp/adminpanel.js
+++ DroneCones/DroneConesApp/static/DroneConesApp/adminpanel.js
@@ -3,13 +3,18 @@ const inventory_button = document.getElementById('Inventory-button');  // button
 const drones_table = document.getElementById('drones');
 const inventory_table = document.getElementById('inventory')
 
+inventory_button.dataset.display="true";
 
 drones_button.addEventListener('click', () => {
     drones_table.dataset.display = "true";
+    drones_button.dataset.display = "true";
     inventory_table.dataset.display = "false";
+    inventory_button.dataset.display = "false";
 });
 
 inventory_button.addEventListener('click', () => {
     drones_table.dataset.display = "false";
+    drones_button.dataset.display = "false";
     inventory_table.dataset.display = "true";
+    inventory_button.dataset.display = "true";
 });
