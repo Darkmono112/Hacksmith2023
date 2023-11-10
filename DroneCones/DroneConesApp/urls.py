33f69f13f91fd73dfa507285e9bc3f8c456c5ce8
zanehirning <a02371568@usu.edu>
2023-11-10 11:38:43 -0700

    removed redundant urls

--- DroneCones/DroneConesApp/urls.py
+++ DroneCones/DroneConesApp/urls.py
@@ -22,8 +22,6 @@ urlpatterns = [
     path("change_password/", account.change_password, name='change_password'),
     path("delete_account/", account.delete_account, name='delete_account'),
     path("checkout/", orders.checkout, name='checkout'),
-    path("set_billing/", orders.set_billing, name='set_billing'),
-    path("set_shipping/", orders.set_shipping, name='set_shipping'),
     path('help/<int:redirect>/', views.faq, name='faq'),
     path('requesthelp/', views.request_help, name='request_help'),
 ]
