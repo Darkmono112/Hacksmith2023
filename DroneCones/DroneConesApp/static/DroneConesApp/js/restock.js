3388d8a43de5034774dfd692526b9f9bc837ab46
zanehirning <a02371568@usu.edu>
2023-11-15 21:50:04 -0700

    Added edit modal

--- DroneCones/DroneConesApp/static/DroneConesApp/js/restock.js
+++ DroneCones/DroneConesApp/static/DroneConesApp/js/restock.js
@@ -30,4 +30,41 @@ form.addEventListener("input", () => {
     if (quantity.value > 0 && item.value != "") {
         updateTotalPrice();
     }
+    else {
+        totalPrice.innerHTML = `${0.00}`;
+    }
+});
+
+let editOpen = false;
+const editModal = document.getElementById("edit-modal");
+const editButton = document.querySelectorAll(".edit-button");
+const editMask = document.getElementById("edit-mask");
+const editName = document.getElementById("edit-name");
+const editPrice = document.getElementById("edit-price");
+const editQuantity = document.getElementById("edit-quantity");
+const editNameInput = document.getElementById("edit-name-input");
+editButton.forEach((button) => {
+    button.addEventListener("click", () => {
+        editOpen = !editOpen
+        editModal.dataset.display = `${editOpen}`;
+        editMask.dataset.display = `${editOpen}`;
+
+        const tableRow = button.closest("tr");
+        const name = tableRow.querySelector("td:nth-child(1)").innerHTML;
+        const price = tableRow.querySelector("td:nth-child(2)").innerHTML;
+        const quantity = tableRow.querySelector("td:nth-child(3)").innerHTML;
+
+        editName.innerHTML = `${name}`;
+        editNameInput.value = `${name}`;
+        editPrice.value = `${parseFloat(price.replace("$", "")).toFixed(2)}`;
+
+        editQuantity.value = `${quantity}`;
+        console.log(editNameInput.value);
+    });
+});
+
+editMask.addEventListener("click", () => {
+    editOpen = !editOpen
+    editModal.dataset.display = `${editOpen}`;
+    editMask.dataset.display = `${editOpen}`;
 });
--- DroneCones/DroneConesApp/views/admin.py
+++ DroneCones/DroneConesApp/views/admin.py
@@ -29,7 +29,10 @@ def adminpanel(request):
     }
 
     if request.method == 'POST':
-        restock_item(request)
+        if request.POST.get("edit-price"):
+            edit_item(request)
+        else:
+            restock_item(request)
     return render(request, 'DroneConesApp/adminpanel.html', context)
 
 def restock_item(request):
@@ -52,3 +55,32 @@ def restock_item(request):
                 topping.quantity = topping.quantity + int(quantity)
                 topping.save()
         return redirect('DroneConesApp:adminpanel')
+    
+def edit_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('edit-name-input')
+        price = request.POST.get('edit-price')
+        quantity = request.POST.get('edit-quantity')
+        print(name)
+        print(price)
+        print(quantity)
+        if "Ice Cream" in name:
+            name = name.removesuffix(" Ice Cream")
+            icecream = Ice_Cream.objects.get(flavor=name)
+            icecream.price = float(price)
+            icecream.quantity = int(quantity)
+            icecream.save()
+        elif "Cone" in name:
+            name = name.removesuffix(" Cone")
+            cone = Cone.objects.get(flavor=name)
+            cone.price = float(price)
+            cone.quantity = int(quantity)
+            cone.save()
+        else:
+            topping = Topping.objects.get(flavor=name)
+            topping.price = price
+            topping.quantity = quantity
+            topping.save()
+        return redirect('DroneConesApp:adminpanel')
+
+
